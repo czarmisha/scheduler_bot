5a15732252a28d11984d2fa0bb84db90692b52a5
Shukrullo <shookrullo@gmail.com>
2022-08-15 07:10:53 +0000

    Update handlers/reserve.py

--- handlers/reserve.py
+++ handlers/reserve.py
@@ -315,5 +315,7 @@ reserve_handler = ConversationHandler(
     conversation_timeout=datetime.timedelta(seconds=60),
     allow_reentry=True,
     run_async=True,
-    per_user=True
+    per_user=True,
+    per_message=True,
+    per_chat=True
 )
