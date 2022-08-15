733b6212b3ed6159e9dd6b5a86f0221e69601501
czar_code <vertigomisha@gmail.com>
2022-08-15 21:41:48 +0500

    local scope at editing

--- handlers/my_events.py
+++ handlers/my_events.py
@@ -37,14 +37,14 @@ def my_events(update: Update, context: CallbackContext):
     if not update.message.chat.type == 'private':
         update.message.reply_text(f"{messages['private_error']['ru']} \n\n {messages['private_error']['uz']}")
         return 
-    global chat_id, events
-    chat_id = update.effective_chat.id
+    # global chat_id, events
+    context.user_data["chat_id"] = update.effective_chat.id
     statement = select(Event).filter(Event.author_id==update.effective_user.id)
     #try?
-    events = local_session.execute(statement).scalars().all()
-    if events:
+    context.user_data["events"] = local_session.execute(statement).scalars().all()
+    if context.user_data["events"]:
         keyboard = [
-            [InlineKeyboardButton(f'{event.description}', callback_data=f"my_event_{ind}")] for ind, event in enumerate(events)
+            [InlineKeyboardButton(f'{event.description}', callback_data=f"my_event_{ind}")] for ind, event in enumerate(context.user_data["events"])
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         print(reply_markup)
@@ -55,13 +55,13 @@ def my_events(update: Update, context: CallbackContext):
         return 
 
 def sel_event(update: Update, context: CallbackContext):
-    global event
+    # global event
     query = update.callback_query
     query.answer()
-    event = events[int(query.data[query.data.rfind('_')+1:])]
+    context.user_data["event"] = context.user_data['events'][int(query.data[query.data.rfind('_')+1:])]
     query.edit_message_text(
-        text=f"✅ {messages['your_select']['ru']}:\n{messages['start']['ru']} {event.start}\n{messages['end']['ru']} {event.end}\n{messages['description']['ru']} {event.description}\n\n{messages['select_action']['ru']}\n\n"\
-            f"✅ {messages['your_select']['uz']}:\n{messages['start']['uz']} {event.start}\n{messages['end']['uz']} {event.end}\n{messages['description']['uz']} {event.description}\n\n{messages['select_action']['uz']}",
+        text=f"✅ {messages['your_select']['ru']}:\n{messages['start']['ru']} {context.user_data['event'].start}\n{messages['end']['ru']} {context.user_data['event'].end}\n{messages['description']['ru']} {context.user_data['event'].description}\n\n{messages['select_action']['ru']}\n\n"\
+            f"✅ {messages['your_select']['uz']}:\n{messages['start']['uz']} {context.user_data['event'].start}\n{messages['end']['uz']} {context.user_data['event'].end}\n{messages['description']['uz']} {context.user_data['event'].description}\n\n{messages['select_action']['uz']}",
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(f"✏️ {messages['edit']['ru']} / {messages['edit']['uz']}", callback_data=f'edit_event')],
@@ -71,71 +71,69 @@ def sel_event(update: Update, context: CallbackContext):
     )
 
 def del_event(update: Update, context: CallbackContext):
-    global event
+    # global event
     query = update.callback_query
     query.answer()
-    local_session.delete(event)
+    local_session.delete(context.user_data["event"])
     local_session.commit()
-    context.bot.send_message(chat_id, f"🗑 {messages['event_is_deleted']['ru']}\n\t\t{messages['event_is_deleted']['uz']}\n\n📝 /reserve \n🖥 /display \n🗃 /my_events")
+    context.bot.send_message(context.user_data['chat_id'], f"🗑 {messages['event_is_deleted']['ru']}\n\t\t{messages['event_is_deleted']['uz']}\n\n📝 /reserve \n🖥 /display \n🗃 /my_events")
 
 def edit_event(update: Update, context: CallbackContext):
-    global event, day, month, year, event_id
+    # global event, day, month, year, event_id
     query = update.callback_query
     query.answer()
-    day = event.start.day
-    month = event.start.month
-    year = event.start.year
-    event_id = event.id
+    context.user_data["day"] = context.user_data["event"].start.day
+    context.user_data["month"] = context.user_data["event"].start.month
+    context.user_data["year"] = context.user_data["event"].start.year
+    context.user_data["event_id"] = context.user_data["event"].id
 
-    str_day = f'0{day}' if day < 10 else day
-    str_month = f'0{month}' if month < 10 else month
+    str_day = f'0{context.user_data["day"]}' if context.user_data["day"] < 10 else context.user_data["day"]
+    str_month = f'0{context.user_data["month"]}' if context.user_data["month"] < 10 else context.user_data["month"]
     query.edit_message_text(text=f"📅 {messages['select_date']['ru']} / {messages['select_date']['uz']}",
                                 reply_markup=InlineKeyboardMarkup(
-                                get_date_keyboard(str_day, str_month, year))
+                                get_date_keyboard(str_day, str_month, context.user_data["year"]))
                             )
     return DATE
 
 def increase_date(update: Update, context: CallbackContext):
     query = update.callback_query
-    logger.info(query.data)
     query.answer()
 
-    global day, month, year
-    if query.data == 'inc_day' and not day == 31:
-        day += 1
-    elif query.data == 'inc_month' and not month == 12:
-        month += 1
+    # global day, month, year
+    if query.data == 'inc_day' and not context.user_data["day"] == 31:
+        context.user_data["day"] += 1
+    elif query.data == 'inc_month' and not context.user_data["month"] == 12:
+        context.user_data["month"] += 1
     elif query.data == 'inc_year':
-        year += 1
+        context.user_data["year"] += 1
 
-    str_day = f'0{day}' if day < 10 else day
-    str_month = f'0{month}' if month < 10 else month
+    str_day = f'0{context.user_data["day"]}' if context.user_data["day"] < 10 else context.user_data["day"]
+    str_month = f'0{context.user_data["month"]}' if context.user_data["month"] < 10 else context.user_data["month"]
     query.edit_message_text(
         text=f"📅 {messages['select_date']['ru']} / {messages['select_date']['uz']}",
         reply_markup=InlineKeyboardMarkup(
-            get_date_keyboard(str_day, str_month, year))
+            get_date_keyboard(str_day, str_month, context.user_data["year"]))
     )
 
 
 def decrease_date(update: Update, context: CallbackContext):
     query = update.callback_query
-    logger.info(query.data)
     query.answer()
 
-    global day, month, year
-    if query.data == 'dec_day' and not day == 1:
-        day -= 1
-    elif query.data == 'dec_month' and not month == 1:
-        month -= 1
-    elif query.data == 'dec_year' and not year == 2022:
-        year -= 1
+    # global day, month, year
+    if query.data == 'dec_day' and not context.user_data["day"] == 1:
+        context.user_data["day"] -= 1
+    elif query.data == 'dec_month' and not context.user_data["month"] == 1:
+        context.user_data["month"] -= 1
+    elif query.data == 'dec_year' and not context.user_data["year"] == 2022:
+        context.user_data["year"] -= 1
 
-    str_day = f'0{day}' if day < 10 else day
-    str_month = f'0{month}' if month < 10 else month
+    str_day = f'0{context.user_data["day"]}' if context.user_data["day"] < 10 else context.user_data["day"]
+    str_month = f'0{context.user_data["month"]}' if context.user_data["month"] < 10 else context.user_data["month"]
     query.edit_message_text(
         text=f"📅 {messages['select_date']['ru']} / {messages['select_date']['uz']}",
         reply_markup=InlineKeyboardMarkup(
-            get_date_keyboard(str_day, str_month, year))
+            get_date_keyboard(str_day, str_month, context.user_data["year"]))
     )
 
 
@@ -143,20 +141,20 @@ def date(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
 
-    global day, hour, minute, event_date
-    event_date = query.data
+    # global day, hour, minute, event_date
+    context.user_data["event_date"] = query.data
 
-    if int(event_date[:2]) < datetime.datetime.now(TZ).day or int(event_date[3:5]) < datetime.datetime.now(TZ).month:
-        global chat_id
+    if int(context.user_data["event_date"][:2]) < datetime.datetime.now(TZ).day or int(context.user_data["event_date"][3:5]) < datetime.datetime.now(TZ).month:
+        # global chat_id
         context.bot.send_message(
-            chat_id=chat_id, text=f"❗️ {messages['date_in_past']['ru']}\n{messages['date_in_past']['uz']}\n\n 📝 /reserve")
+            chat_id=context.user_data["chat_id"], text=f"❗️ {messages['date_in_past']['ru']}\n{messages['date_in_past']['uz']}\n\n 📝 /reserve")
         return ConversationHandler.END
 
-    hour = datetime.datetime.now(TZ).hour if datetime.datetime.now(TZ).hour < 20 or datetime.datetime.now(TZ).hour > 8 else 8
-    minute = event.start.minute
+    context.user_data["hour"] = datetime.datetime.now(TZ).hour if datetime.datetime.now(TZ).hour < 20 or datetime.datetime.now(TZ).hour > 8 else 8
+    context.user_data["minute"] = context.user_data["event"].start.minute
 
-    str_min = f'0{minute}' if minute < 10 else minute
-    str_h = f'0{hour}' if hour < 10 else hour
+    str_min = f'0{context.user_data["minute"]}' if context.user_data["minute"] < 10 else context.user_data["minute"]
+    str_h = f'0{context.user_data["hour"]}' if context.user_data["hour"] < 10 else context.user_data["hour"]
     query.edit_message_text(
         text=f"{messages['select_start_time']['ru']} / {messages['select_start_time']['uz']}",
         reply_markup=InlineKeyboardMarkup(
@@ -167,31 +165,29 @@ def date(update: Update, context: CallbackContext):
 
 
 def increase_time(update: Update, context: CallbackContext):
-    # может время по 15мин сделать а не по 5
     query = update.callback_query
-    logger.info(query.data)
     query.answer()
 
-    global hour, minute
+    # global hour, minute
     if query.data.startswith('inc_hour'):
-        if hour >= 20:
-            hour = 8
+        if context.user_data["hour"] >= 20:
+            context.user_data["hour"] = 8
         else:
-            hour += 1
+            context.user_data["hour"] += 1
     elif query.data.startswith('inc_minute'):
-        if not minute % 5 == 0:
-            minute = minute + (5 - minute % 5)
-            if minute == 60:
-                minute = 0
-        elif minute == 59:
-            minute = 0
+        if not context.user_data["minute"] % 5 == 0:
+            context.user_data["minute"] = context.user_data["minute"] + (5 - context.user_data["minute"] % 5)
+            if context.user_data["minute"] == 60:
+                context.user_data["minute"] = 0
+        elif context.user_data["minute"] == 59:
+            context.user_data["minute"] = 0
         else:
-            minute += 5
-            if minute >= 59:
-                minute = 0
+            context.user_data["minute"] += 5
+            if context.user_data["minute"] >= 59:
+                context.user_data["minute"] = 0
 
-    str_min = f'0{minute}' if minute < 10 else minute
-    str_h = f'0{hour}' if hour < 10 else hour
+    str_min = f'0{context.user_data["minute"]}' if context.user_data["minute"] < 10 else context.user_data["minute"]
+    str_h = f'0{context.user_data["hour"]}' if context.user_data["hour"] < 10 else context.user_data["hour"]
     txt = f"🕔 {messages['select_start_time']['ru']} / {messages['select_start_time']['uz']}" if query.data.endswith('start') else f"{messages['select_end_time']['ru']} / {messages['select_end_time']['uz']}"
     query.edit_message_text(
         text=txt,
@@ -206,22 +202,22 @@ def decrease_time(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
 
-    global hour, minute
+    # global hour, minute
     if query.data.startswith('dec_hour'):
-        if hour <= 8:
-            hour = 20
+        if context.user_data["hour"] <= 8:
+            context.user_data["hour"] = 20
         else:
-            hour -= 1
+            context.user_data["hour"] -= 1
     elif query.data.startswith('dec_minute'):
-        if not minute % 5 == 0:
-            minute = minute - (minute % 5)
-        elif minute == 0:
-            minute = 55
+        if not context.user_data["minute"] % 5 == 0:
+            context.user_data["minute"] = context.user_data["minute"] - (context.user_data["minute"] % 5)
+        elif context.user_data["minute"] == 0:
+            context.user_data["minute"] = 55
         else:
-            minute -= 5
+            context.user_data["minute"] -= 5
 
-    str_min = f'0{minute}' if minute < 10 else minute
-    str_h = f'0{hour}' if hour < 10 else hour
+    str_min = f'0{context.user_data["minute"]}' if context.user_data["minute"] < 10 else context.user_data["minute"]
+    str_h = f'0{context.user_data["hour"]}' if context.user_data["hour"] < 10 else context.user_data["hour"]
     txt = f"🕔 {messages['select_start_time']['ru']} / {messages['select_start_time']['uz']}" if query.data.endswith('start') else f"{messages['select_end_time']['ru']} / {messages['select_end_time']['uz']}"
     query.edit_message_text(
         text=txt,
@@ -236,12 +232,12 @@ def start(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
 
-    global hour, minute, event_start
-    event_start = query.data
-    hour += 1
+    # global hour, minute, event_start
+    context.user_data["event_start"] = query.data
+    context.user_data["hour"] += 1
 
-    str_min = f'0{minute}' if minute < 10 else minute
-    str_h = f'0{hour}' if hour < 10 else hour
+    str_min = f'0{context.user_data["minute"]}' if context.user_data["minute"] < 10 else context.user_data["minute"]
+    str_h = f'0{context.user_data["hour"]}' if context.user_data["hour"] < 10 else context.user_data["hour"]
     query.edit_message_text(
         text=f"🕔 {messages['select_end_time']['ru']} / {messages['select_end_time']['uz']}",
         reply_markup=InlineKeyboardMarkup(
@@ -255,20 +251,20 @@ def end(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
 
-    global chat_id, event_end
-    event_end = query.data
-    context.bot.send_message(chat_id=chat_id, text=f"🖊 {messages['input_description']['ru']} / {messages['input_description']['uz']}")
+    # global chat_id, event_end
+    context.user_data["event_end"] = query.data
+    context.bot.send_message(chat_id=context.user_data["chat_id"], text=f"🖊 {messages['input_description']['ru']} / {messages['input_description']['uz']}")
 
     return DESCRIPTION
 
 
 def description(update: Update, context: CallbackContext):
-    global event_date, event_start, event_end
+    # global event_date, event_start, event_end
     
-    event_start = datetime.datetime.strptime(event_date + ' ' + event_start, '%d.%m.%Y %H:%M')
-    event_end = datetime.datetime.strptime(event_date + ' ' + event_end, '%d.%m.%Y %H:%M')
+    context.user_data["event_start"] = datetime.datetime.strptime(context.user_data["event_date"] + ' ' + context.user_data["event_start"], '%d.%m.%Y %H:%M')
+    context.user_data["event_end"] = datetime.datetime.strptime(context.user_data["event_date"] + ' ' + context.user_data["event_end"], '%d.%m.%Y %H:%M')
 
-    validator = EventValidator(event_start, event_end, update.message.text)
+    validator = EventValidator(context.user_data["event_start"], context.user_data["event_end"], update.message.text)
     success, mess = validator.duration_validation()
     if not success:
         logger.error(mess)
@@ -285,7 +281,7 @@ def description(update: Update, context: CallbackContext):
         )
         return START
     
-    collision = validator.collision_validation(edit=True, event_id=event_id)
+    collision = validator.collision_validation(edit=True, event_id=context.user_data["event_id"])
     if not collision[0]:
         logger.error(collision[1])
         update.message.reply_text(collision[1])
@@ -301,18 +297,18 @@ def description(update: Update, context: CallbackContext):
         )
         return START
 
-    event.description = update.effective_message.text
-    event.start = event_start
-    event.end = event_end
+    context.user_data["event"].description = update.effective_message.text
+    context.user_data["event"].start = context.user_data["event_start"]
+    context.user_data["event"].end = context.user_data["event_end"]
     local_session.commit()
     update.message.reply_text(f"✏️✅ {messages['event_is_edited']['ru']}\n\t\t{messages['event_is_edited']['uz']}\n\n📝 /reserve \n🖥 /display \n🗃 /my_events")
     return ConversationHandler.END
 
 
 def cancel(update: Update, context: CallbackContext):
-    global chat_id
+    # global chat_id
     context.bot.send_message(
-        chat_id=chat_id, text=f"{messages['canceled']['ru']}\n\t\t{messages['canceled']['uz']}\n\n📝 /reserve \n🖥 /display \n🗃 /my_events")
+        chat_id=context.user_data["chat_id"], text=f"{messages['canceled']['ru']}\n\t\t{messages['canceled']['uz']}\n\n📝 /reserve \n🖥 /display \n🗃 /my_events")
     return ConversationHandler.END
 
 edit_handler = ConversationHandler(
