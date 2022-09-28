e9285bff0373ddd21a7ba845f092ce99980047be
czar_code <vertigomisha@gmail.com>
2022-09-28 11:00:11 +0500

    add recognition car plate num with api

---
+++ handlers/car_detect.py
@@ -0,0 +1,54 @@
+import os, requests, base64
+from datetime import datetime
+from telegram import Update
+from telegram.ext import CallbackContext, MessageHandler, Filters
+
+from sqlalchemy import select
+from db.models import Group, Session, engine
+from utils.translation import messages
+
+local_session = Session(bind=engine)
+
+def get_plate_numbers(file_path):
+    with open(file_path, "rb") as image:
+            img_b64 = base64.b64encode(image.read())
+            url = 'https://api.platerecognizer.com/v1/plate-reader/'
+            data={
+                'regions': 'uz',
+                'upload': img_b64
+            }
+            headers = {
+                "Authorization": "Token dc4b9b659547e7155264e7dfd0e8dcb6974e7a88"
+            }
+            response = requests.post(url=url, headers=headers, data=data)
+            if response.ok:
+                return (True, response.json()['results'])
+            else:
+                return (False, response.text)
+
+def find_in_db(plate_num):
+    # local_session.query(Car).filter(Car.plate=plate_num).first()
+    pass
+
+def car_detect(update: Update, context: CallbackContext):
+    
+    statement = select(Group)
+    group = local_session.execute(statement).scalars().first()
+    author = context.bot.get_chat_member(group.tg_id, update.effective_user.id)
+    if author.status == 'left' or author.status == 'kicked' or not author.status:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f"{messages['auth_err']['ru']} / {messages['auth_err']['uz']}")
+    else:
+        file = context.bot.getFile(update.message.photo[1].file_id)
+        file_path = f'media/images/{update.effective_user.id}-{datetime.now().strftime("%Y%m%d%H%M")}.jpg'
+        file.download(file_path)
+
+        plate_nums = get_plate_numbers(file_path)
+        if not plate_nums[0]:
+            return
+        for num in plate_nums[1]:
+            update.message.reply_text(f"Номер машины с фото: {num['plate']}\nБазы дынных пока нет")
+        
+        os.remove(file_path)
+
+detect_handler = MessageHandler(Filters.photo, car_detect)
