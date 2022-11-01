72ce8ba24271928d53c000c0d728020cb7da14a0
Mihail <vertigomisha@gmail.com>
2022-11-01 10:11:32 +0500

    fix

--- handlers/repair.py
+++ handlers/repair.py
@@ -1,3 +1,4 @@
+import logging
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext, CommandHandler
 
@@ -5,9 +6,13 @@ from db.models import Session, engine
 
 
 local_session = Session(bind=engine)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def repair(update: Update, context: CallbackContext):
-    print(update)
-    print(update.message.migrate_from_chat_id)
+    logger.info('!!!!!!!', update)
+    logger.info('!!!!!!!', update.effective_chat.id)
 
 repair_handler = CommandHandler('repair', repair)
